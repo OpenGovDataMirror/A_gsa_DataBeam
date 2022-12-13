29f90842372d40f979ea385836d592bdbd81518d
ep-infosec <ep-infosec@users.noreply.github.com>
2022-12-14 01:57:35 +0200

    init

---
+++ application/config/sample-env/upload.php
@@ -0,0 +1,11 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+/*
+|--------------------------------------------------------------------------
+| Upload Arguments
+|--------------------------------------------------------------------------
+*/
+
+$config['upload_path'] = '/your/file/path/'; // absolute path
+$config['allowed_types'] = 'csv'; //'gif|jpg|png|csv|txt|JPG|GIF|PNG|CSV|TXT';
+$config['max_size'] = '2000'; // kb
